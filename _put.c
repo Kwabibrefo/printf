2820e29cc16add37eeeac4823eb0bf7a4e62adbf
frontpro <aly.arbab@gmail.com>
2023-04-28 02:21:08 -0700

    printf

--- _put.c
+++ _put.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 /**
  * _puts - print string
  * @str: string
--- main.h
+++ main.h
@@ -68,16 +68,16 @@ int print_number_left_shift(char *str, params_t *params);
 */
 typedef struct parameters
 {
-unsigned int unsign :1;
-unsigned int plus_flag :1;
-unsigned int space_flag :1;
-unsigned int hashtag_flag :1;
-unsigned int zero_flag :1;
-unsigned int minus_flag :1;
-unsigned int h_modifier :1;
-unsigned int l_modifier :1;
-unsigned int width;
-unsigned int percision;
+	unsigned int unsign		: 1;
+	unsigned int plus_flag		: 1;
+	unsigned int space_flag		: 1;
+	unsigned int hashtag_flag	: 1;
+	unsigned int zero_flag		: 1;
+	unsigned int minus_flag		: 1;
+	unsigned int h_modifier		: 1;
+	unsigned int l_modifier		: 1;
+	unsigned int width;
+	unsigned int percision;
 } params_t;
 
 /**
