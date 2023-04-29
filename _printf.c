623267b3b3ad9c95e3cfd12b6fe3e0e01b7c379d
Alyaa <33777987+frontpro@users.noreply.github.com>
2023-04-29 07:00:58 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,4 +1,7 @@
++#include <stdio.h>
+#include <stdarg.h>
 #include "main.h"
+#include <stdarg.h>
 
 /**
  * _printf - function to print strings
@@ -6,45 +9,48 @@
  * Return: returns count of chars printed except null byte
  */
 
-int _printf(const char *format, ...)
+int _printf(const char *format, ...);
 {
-int counter = 0;
-const char *ptr;
-char *start;
-va_list ap;
-params_t params = PARAMS_INIT;
+	va_list ap;
+	int counter;
 
-va_start(ap, format);
-
-if (!format || (format[0] == '%' && !format[1]))
-return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2])
-return (-1);
-for (ptr = (char *)format; *ptr; ptr++)
-{
-init_params(&params, ap);
-if (*ptr != '%')
-{
-counter += _putchar(*ptr);
-continue;
-}
-start = ptr;
-ptr++;
-while (get_flag(ptr, &params))
-{
-ptr++;
-}
-ptr = get_width(ptr, &params, ap);
-ptr = get_precision(ptr, &params, ap);
-if (get_modifier(ptr, &params))
-ptr++;
-if (!get_specifier(ptr))
-counter += print_from_to(start, ptr,
-			 params.l_modifier || params.h_modifier ? ptr - 1 : 0);
-else
-counter += get_print_func(ptr, ap, &params);
-}
-_putchar(BUF_FLUSH);
-va_end(ap);
-return (counter);
+	va_start(ap, counter);
+	for (format; *format; format++)
+	{
+		if (*format == %)
+		{
+			format++;
+			 if (*format == 'c')
+			 {
+				 char c = va_arg(ap, int);
+				 putchar('c')
+				 count++;
+			 }
+			 if (*format == %)
+			 {
+				 putchar(37);
+				 count++;
+			 }
+			 if (*format == s)
+			 {
+				 char *str = va_arg(args, char *);
+				 for (i = 0; str[i] != '\0'; i++)
+				 {
+					 putchar('str[i]');
+					 count++;
+				 }
+			 }
+			else
+			{
+				return (-1);
+			}
+		}
+		else
+		{
+			write(STDOUT_FILENO, format, 1);
+			count++;
+		}
+		va_end (ap);
+		return (count);
+	}
 }
